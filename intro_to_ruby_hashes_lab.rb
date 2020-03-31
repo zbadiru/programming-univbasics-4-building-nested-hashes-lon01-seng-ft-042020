8b474fdbb3a3dc660fbe1a0a23a744362ac06597
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:01:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,10 @@ monopoly = {:railroads => {}}
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-    {:pieces => 4}, 
-    {:rent_in_dollars = {}}, 
-    {:name => {}}
+  monopoly = {:railroads => {
+    {pieces: => 4}, 
+    {rent_in_dollars: = {}}, 
+    {name: => {}}
     }
   }
 
