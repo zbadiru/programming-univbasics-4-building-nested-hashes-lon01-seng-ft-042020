0ab47d865f5cbd3b3e53b7935bc827761146777b
Zakari Badiru <zbadiru@live.co.uk>
2020-03-31 14:19:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,9 +20,9 @@ monopoly =  {
     :pieces=>4,
     :rent_in_dollars=>{
       :one_piece_owned=>25,
-      :two_piece_owned=>50,
-      :three_piece_owned=>100,
-      :four_piece_owned=>200
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
     },
     :names=>{
       :reading_railroad=>{},
